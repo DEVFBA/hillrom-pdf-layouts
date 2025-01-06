1f4f25204fe979b7438082b9ad4360ad0f47a2a5
angelagar <ggarcia.angelad@gmail.com>
2025-01-05 19:16:58 -0600

    update: TyC se añaden a peticion

--- controllers/generate-pdf.js
+++ controllers/generate-pdf.js
@@ -5,6 +5,7 @@ var gruas = require("../gruas");
 var physician = require("../physician");
 const dbGeneralParameters= require('./cat-general-parameters');
 const dbZones = require('./cat-zones');
+const getThermsOfDistribution = require('../layoutsPhysician/ThermsOfDistribution')
 
 async function createPdf(params){
     try{
@@ -27,112 +28,179 @@ async function createPdf(params){
                 } 
                 return response
             }
+            // else {
+            //     async  
+            //     var idZoneType = pdf.recordsets[0][0].Id_Zone_Type
+            //     var priceList = params.priceList;
+            //     var pdfVersion = params.pdfVersion;
+            //     var printTermsConditions = params.printTermsConditions;
+            //     try {
+            //         let listPrice = await pool.request()
+            //             .input('pvIdLayoutType', sql.VarChar, params.priceList)
+            //             .input('pvIdZoneType', sql.VarChar, idZoneType)
+            //             .input('pvIdZone', sql.VarChar, params.pdfZone)
+            //             .execute('spPDF_Prices_List_Get_Layouts')
+
+            //         if(listPrice.recordsets[0].length === 0)
+            //         {
+            //             return response = {
+            //                 data: {
+            //                     status: 0,
+            //                     message: "Lista de Precios o Zona de PDF inválidas",
+            //                     pdfPath: ""
+            //                 }
+            //             }
+            //         }
+            //         else {
+            //             if(priceList === "IPL")
+            //             {
+            //                 const fecha = new Date();
+            //                 var anio = fecha.getFullYear()
+            //                 var mes; 
+            //                 var dia;
+            //                 if((fecha.getMonth() + 1) < 10)
+            //                 {
+            //                     mes = "0" + (fecha.getMonth() + 1)
+            //                 }
+            //                 else {
+            //                     mes = fecha.getMonth() + 1
+            //                 }
+
+            //                 if((fecha.getDate()) < 10)
+            //                 {
+            //                     dia = "0" + (fecha.getDate())
+            //                 }
+            //                 else {
+            //                     dia = fecha.getDate()
+            //                 }
+            //                 var hora = fecha.getHours()
+            //                 var minutos = fecha.getMinutes()
+            //                 var segundos = fecha.getSeconds()
+                    
+            //                 var ruta = await dbGeneralParameters.getGeneralParametersbyID("55")
+            //                 var rutaPdf = ruta[0][0].Value
+
+            //                 var rutaIp = await dbGeneralParameters.getGeneralParametersbyID("56")
+            //                 var rutaPdfIp = rutaIp[0][0].Value
+
+            //                 var zone = await dbZones.getZones(params.pdfZone)
+            //                 var nombreArchivo = zone[0][0].Short_Desc + "-" + priceList + "-" + "V" + pdfVersion + "_" + anio + mes + dia + hora + minutos + segundos
+            //                 var zonaNombre = zone[0][0].Short_Desc
+            //                 return await camas.createDocument(rutaPdf, rutaPdfIp, nombreArchivo, zonaNombre, pdfVersion, listPrice.recordsets[0])
+            //             }
+            //             else {
+            //                 const fecha = new Date();
+            //                 var anio = fecha.getFullYear();
+            //                 var mes; 
+            //                 var dia;
+            //                 if((fecha.getMonth() + 1) < 10)
+            //                 {
+            //                     mes = "0" + (fecha.getMonth() + 1);
+            //                 }
+            //                 else {
+            //                     mes = fecha.getMonth() + 1;
+            //                 }
+
+            //                 if((fecha.getDate()) < 10)
+            //                 {
+            //                     dia = "0" + (fecha.getDate());
+            //                 }
+            //                 else {
+            //                     dia = fecha.getDate();
+            //                 }
+            //                 var hora = fecha.getHours();
+            //                 var minutos = fecha.getMinutes();
+            //                 var segundos = fecha.getSeconds();
+                    
+            //                 var ruta = await dbGeneralParameters.getGeneralParametersbyID("55");
+            //                 var rutaPdf = ruta[0][0].Value;
+
+            //                 var rutaIp = await dbGeneralParameters.getGeneralParametersbyID("56");
+            //                 var rutaPdfIp = rutaIp[0][0].Value;
+
+            //                 var zone = await dbZones.getZones(params.pdfZone);
+            //                 var nombreArchivo = zone[0][0].Short_Desc + "-" + priceList + "-" + "V" + pdfVersion + "_" + anio + mes + dia + hora + minutos + segundos
+            //                 var zonaNombre = zone[0][0].Short_Desc;
+            //                 return await gruas.createDocument(rutaPdf, rutaPdfIp, nombreArchivo, zonaNombre, pdfVersion, listPrice.recordsets[0], params.pdfZone);
+            //             }
+            //         }
+                   
+            //     }catch(error){
+            //         return response = {
+            //             data: {
+            //                 status: 0,
+            //                 message: "Lista de Precios o Zona de PDF inválidas",
+            //                 pdfPath: ""
+            //             }
+            //         }
+            //     }
+            // }
             else {
-                var idZoneType = pdf.recordsets[0][0].Id_Zone_Type
+                const TyC = await getThermsOfDistribution(); 
+                var idZoneType = pdf.recordsets[0][0].Id_Zone_Type;
                 var priceList = params.priceList;
                 var pdfVersion = params.pdfVersion;
+                var printTermsConditions = params.printTermsConditions;
                 try {
-                
-                    let listPrice = await pool.request()
+                    let request = pool.request()
                         .input('pvIdLayoutType', sql.VarChar, params.priceList)
                         .input('pvIdZoneType', sql.VarChar, idZoneType)
-                        .input('pvIdZone', sql.VarChar, params.pdfZone)
-                        .execute('spPDF_Prices_List_Get_Layouts')
-
-                    if(listPrice.recordsets[0].length === 0)
-                    {
+                        .input('pvIdZone', sql.VarChar, params.pdfZone);
+            
+                    if (printTermsConditions) {
+                        request = request.input('pvTermsConditions', sql.VarChar, TyC);
+                    }
+                    let listPrice = await request.execute('spPDF_Prices_List_Get_Layouts');
+            
+                    if (listPrice.recordsets[0].length === 0) {
                         return response = {
                             data: {
                                 status: 0,
                                 message: "Lista de Precios o Zona de PDF inválidas",
                                 pdfPath: ""
                             }
+                        };
+                    } else {
+                        const fecha = new Date();
+                        var anio = fecha.getFullYear();
+                        var mes;
+                        var dia;
+                        if ((fecha.getMonth() + 1) < 10) {
+                            mes = "0" + (fecha.getMonth() + 1);
+                        } else {
+                            mes = fecha.getMonth() + 1;
                         }
-                    }
-                    else {
-                        if(priceList === "IPL")
-                        {
-                            const fecha = new Date();
-                            var anio = fecha.getFullYear()
-                            var mes; 
-                            var dia;
-                            if((fecha.getMonth() + 1) < 10)
-                            {
-                                mes = "0" + (fecha.getMonth() + 1)
-                            }
-                            else {
-                                mes = fecha.getMonth() + 1
-                            }
-
-                            if((fecha.getDate()) < 10)
-                            {
-                                dia = "0" + (fecha.getDate())
-                            }
-                            else {
-                                dia = fecha.getDate()
-                            }
-                            var hora = fecha.getHours()
-                            var minutos = fecha.getMinutes()
-                            var segundos = fecha.getSeconds()
-                    
-                            var ruta = await dbGeneralParameters.getGeneralParametersbyID("55")
-                            var rutaPdf = ruta[0][0].Value
-
-                            var rutaIp = await dbGeneralParameters.getGeneralParametersbyID("56")
-                            var rutaPdfIp = rutaIp[0][0].Value
-
-                            var zone = await dbZones.getZones(params.pdfZone)
-                            var nombreArchivo = zone[0][0].Short_Desc + "-" + priceList + "-" + "V" + pdfVersion + "_" + anio + mes + dia + hora + minutos + segundos
-                            var zonaNombre = zone[0][0].Short_Desc
-                            return await camas.createDocument(rutaPdf, rutaPdfIp, nombreArchivo, zonaNombre, pdfVersion, listPrice.recordsets[0])
+            
+                        if ((fecha.getDate()) < 10) {
+                            dia = "0" + (fecha.getDate());
+                        } else {
+                            dia = fecha.getDate();
                         }
-                        else {
-                            const fecha = new Date();
-                            var anio = fecha.getFullYear();
-                            var mes; 
-                            var dia;
-                            if((fecha.getMonth() + 1) < 10)
-                            {
-                                mes = "0" + (fecha.getMonth() + 1);
-                            }
-                            else {
-                                mes = fecha.getMonth() + 1;
-                            }
-
-                            if((fecha.getDate()) < 10)
-                            {
-                                dia = "0" + (fecha.getDate());
-                            }
-                            else {
-                                dia = fecha.getDate();
-                            }
-                            var hora = fecha.getHours();
-                            var minutos = fecha.getMinutes();
-                            var segundos = fecha.getSeconds();
-                    
-                            var ruta = await dbGeneralParameters.getGeneralParametersbyID("55");
-                            var rutaPdf = ruta[0][0].Value;
-
-                            var rutaIp = await dbGeneralParameters.getGeneralParametersbyID("56");
-                            var rutaPdfIp = rutaIp[0][0].Value;
-
-                            var zone = await dbZones.getZones(params.pdfZone);
-                            var nombreArchivo = zone[0][0].Short_Desc + "-" + priceList + "-" + "V" + pdfVersion + "_" + anio + mes + dia + hora + minutos + segundos
-                            var zonaNombre = zone[0][0].Short_Desc;
+                        var hora = fecha.getHours();
+                        var minutos = fecha.getMinutes();
+                        var segundos = fecha.getSeconds();
+            
+                        // Obtener las rutas de los parámetros generales
+                        var ruta = await dbGeneralParameters.getGeneralParametersbyID("55");
+                        var rutaPdf = ruta[0][0].Value;
+            
+                        var rutaIp = await dbGeneralParameters.getGeneralParametersbyID("56");
+                        var rutaPdfIp = rutaIp[0][0].Value;
+            
+                        var zone = await dbZones.getZones(params.pdfZone);
+                        var nombreArchivo = zone[0][0].Short_Desc + "-" + priceList + "-" + "V" + pdfVersion + "_" + anio + mes + dia + hora + minutos + segundos;
+                        var zonaNombre = zone[0][0].Short_Desc;
+            
+                        if (priceList === "IPL") {
+                            return await camas.createDocument(rutaPdf, rutaPdfIp, nombreArchivo, zonaNombre, pdfVersion, listPrice.recordsets[0]);
+                        } else {
                             return await gruas.createDocument(rutaPdf, rutaPdfIp, nombreArchivo, zonaNombre, pdfVersion, listPrice.recordsets[0], params.pdfZone);
                         }
                     }
-                   
-                }catch(error){
-                    return response = {
-                        data: {
-                            status: 0,
-                            message: "Lista de Precios o Zona de PDF inválidas",
-                            pdfPath: ""
-                        }
-                    }
+                } catch(error){
+                    console.log('Error al generar PDF', error)
                 }
-            }
+            }            
     }catch(error){
         console.log(error)
         return response = {
